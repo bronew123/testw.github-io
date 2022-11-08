764e753fa0b2cab6df62f654f3637852ae2b22e7
bronew123 <browhat1236@gmail.com>
2022-11-08 13:33:38 +0530

    testing

---
+++ listener.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+
+//var bodyParser = require("body-parser");
+//var parser = bodyParser.urlencoded({extended:false});
+//listen
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(3000);
+
+app.get("/", function (req, res) {
+  res.send("<h1>Test</h1>");
+});
+
+app.post("/", function (req, res) {
+  console.log(req.body);
+  console.log("Post received");
+  res.send(req.body);
+});
+
+//sending a request
+
+await fetch(window.location.href, {
+  // Adding method type
+  method: "POST",
+
+  // Adding body or contents to send
+  body: JSON.stringify({
+    title: "foo",
+    body: "bar",
+    userId: 1,
+  }),
+
+  // Adding headers to the request
+  headers: {
+    "Content-type": "application/json; charset=UTF-8",
+  },
+});
+
+//
