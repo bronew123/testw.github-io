27bf38b5d6bd9f4bec722621b27562487c21727a
bronew123 <browhat1236@gmail.com>
2022-11-08 14:00:28 +0530

    f

--- listener.js
+++ listener.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(cors({
-  origin:'*'
+  origin:'https://www.youtube.com/watch?v=PNtFSVU-YTI'
 }))
 app.get("/", function (req, res) {
   res.send("<h1>Test</h1>");
